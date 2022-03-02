a7d5b6f17a1c8b196624f846fe5dddfcfd26f3af
Rob <letruthful@gmail.com>
2022-03-02 18:41:39 +0000

    Added button functionality, stored value and operator, value parsing

--- app.js
+++ app.js
@@ -1,4 +1,104 @@
-// Basic math functions
+const buttons = document.querySelectorAll('button');
+const screenCurrentVal = document.querySelector('.value');
+const screenStoredVal = document.querySelector('.stored');
+
+let storedVal = 0;
+let currentVal = 0;
+let operator = "";
+let displayVal = "";
+let isDecimalDisabled = false;
+
+function init() {
+    buttons.forEach(button => {
+        button.addEventListener("click", e => {
+            const clicked = e.target.dataset;
+            if (clicked?.input) {
+                updateDisplay(clicked.input);
+            } else if (clicked?.task) {
+                performTask(clicked.task)
+            } else if (clicked?.func) {
+                addOperator(clicked.func);
+            } else {
+                console.log("Unknown input");
+            }         
+        })
+    })
+
+    screenCurrentVal.textContent = currentVal;
+    screenStoredVal.textContent = " ";
+}
+
+function updateDisplay(input) {
+    if (isDecimalDisabled && input === ".") {
+        return
+    } else {
+        if (input === ".") {
+            isDecimalDisabled = true;
+        }
+        displayVal += input;
+        screenCurrentVal.textContent = displayVal;
+    }
+    
+}
+
+function updateStoredDisplay() {
+    let operatorSymbol = "";
+    switch(operator) {
+        case "add":
+            operatorSymbol = "+";
+            break;
+        case "subtract":
+            operatorSymbol = "-";
+            break;
+        case "multiply":
+            operatorSymbol = "x";
+            break;
+        case "divide":
+            operatorSymbol = "÷";
+            break;
+        default:
+            console.error("Unknown operator");
+            operatorSymbol = "";
+    }
+    screenStoredVal.textContent = `${storedVal} ${operatorSymbol}`;
+}
+
+function addOperator(input) {
+    storedVal = Number.parseFloat(displayVal);
+    operator = input;
+    displayVal = "";
+    isDecimalDisabled = false;
+    updateStoredDisplay();
+
+    console.log(storedVal);
+    console.log(operator);
+}
+
+function performTask(task) {
+    switch(task) {
+        case "clear":
+            clearAll();
+            break;
+        case "backspace":
+            break;
+        case "operate":
+            break;
+    }
+}
+
+// Tasks
+
+function clearAll() {
+    displayVal = "";
+    storedVal = 0;
+    currentVal = 0;
+    operator = "";
+    isDecimalDisabled = false;
+    screenCurrentVal.textContent = currentVal;
+    screenStoredVal.textContent = " ";
+}
+
+
 
 function operate(operator, a, b) {
     switch(operator) {
@@ -11,7 +111,7 @@ function operate(operator, a, b) {
         case "divide":
             return divide(a, b);
         default:
-            console.log("No operator");
+            console.error("Unknown operator");
     }
 }
 
@@ -30,3 +130,5 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+init();
